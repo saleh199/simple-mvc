f17403dea69cbc72023a47124c24390e6c368173
Saleh Saiid <saleh.saiid@gmail.com>
2015-04-09 20:46:32 +0300

    organize application's directory

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./application/routes/index');
+var users = require('./application/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/application/', 'views'));
 app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public

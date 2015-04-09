1de3d4ec22f934a8821f2a6fac3d92fa04ef31c7
Saleh Saiid <saleh.saiid@gmail.com>
2015-04-09 21:50:43 +0300

    remove cookies parser

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./application/routes/index');
@@ -21,7 +21,7 @@ app.set('view engine', 'hjs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);

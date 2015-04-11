aaf153b77e29fa8543984bcf4f0f12d045adac50
Saleh Saiid <saleh.saiid@gmail.com>
2015-04-11 03:20:25 +0300

    add test controller

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
+var test = require('../controllers/test');
+
 module.exports = function(app){
   /* GET home page. */
   app.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+  });
+
+  app.get('/test', function(req, res){
+    res.jsonp(test.all());
   });
 };
